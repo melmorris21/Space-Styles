178f8cb84ecda6469472d104fae6c989c15ab567
Melissa Morris <mel.morris2021@gmail.com>
2023-10-18 14:17:50 -0400

    Images, Styling, and Site Structure

---
+++ spacestyles.js
@@ -0,0 +1,12 @@
+//Display Light, Dark, or Colorful Color Scheme Modes
+function lightMode(){
+    document.getElementById("styles").setAttribute('href', './global.css');
+ }
+ function darkMode(){
+    document.getElementById("styles").setAttribute('href', './dark.css');
+ }
+ function galaxyMode(){
+    document.getElementById("styles").setAttribute('href', './galaxy.css');
+ }
+
+
--- stylechange.js
+++
@@ -1,3 +0,0 @@
-function colorMode(){
-    document.getElementById("styles").setAttribute('href', './analogous.css');
- }
