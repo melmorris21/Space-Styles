eae176b2ae80a2e2a0de8c997b2e1dcbaee6fcb1
Melissa Morris <mel.morris2021@gmail.com>
2023-10-20 16:45:38 -0400

    Home Page Design Changes

--- spacestyles.js
+++ spacestyles.js
@@ -1,12 +1,9 @@
-//Display Light, Dark, or Colorful Color Scheme Modes
-function lightMode(){
-    document.getElementById("styles").setAttribute('href', './global.css');
- }
- function darkMode(){
-    document.getElementById("styles").setAttribute('href', './dark.css');
- }
- function galaxyMode(){
-    document.getElementById("styles").setAttribute('href', './galaxy.css');
- }
+// Get the earth quiz
+var modal = document.getElementById('earthquiz');
 
-
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
